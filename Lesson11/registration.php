fbee89dd926631a1b3b3953ab662fa0ac87f2681
Brad <bskaehler@my.waketech.edu>
2025-04-07 15:22:10 -0400

    Lesson 12
    Completed lab 12 and added functionality to registration.php

--- Lesson11/registration.php
+++ Lesson11/registration.php
@@ -11,11 +11,34 @@
       $fname = $_POST['fname'];
       $lname = $_POST['lname'];
       $numCourse = $_POST['numCourse'];
+      $major = $_POST['major'];
+      $status = $_POST['status'];
+	   $CSC120 = $_POST['CSC120'];
+	   $CSC121 = $_POST['CSC121'];
+	   $CSC154 = $_POST['CSC154'];
 
-      $tuition = $numCourse * 200.00;
+      if($status == "InState") {
+         $tuition = $numCourse * 200.00;
+      }
+      else {
+         $tuition = $numCourse * 500.00;
+      }
 
       print("<p>$fname $lname</p>");
+      print("<p>Your major is $major.</p>");
+      print("<p>You are $status student.</p>");
+      
       print("<p>You have registered $numCourse courses.</p>");
+      if(isset($_POST['CSC120'])){
+         print("CSC120 Computing Fundamentals<br>");
+      }
+      if(isset($_POST['CSC121'])){
+         print("CSC121 Python Programming<br>");
+      }
+      if(isset($_POST['CSC154'])){
+         print("CSC154 Software Development<br>");
+      }   
+
       print("<p>Your tuition is $".number_format($tuition,2).".</p>");
    ?>
 
